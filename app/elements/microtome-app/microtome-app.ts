2f43f3bf9957df781cf0bc606a0ef08247112565
Daniel Joyce <daniel.a.joyce@gmail.com>
2016-01-01 17:34:40 -0800

    Moved app code into microtome tag. Removed unused files.

---
+++ app/elements/microtome-app/microtome-app.ts
@@ -0,0 +1,17 @@
+
+@component("microtome-app")
+class MicrotomeApp extends polymer.Base{
+
+    onMenuActivate(event: Event, detail: Object) {
+      var menu = Polymer.dom(event).localTarget;
+      menu.select(-1);
+    }
+    
+    ready()
+    {
+      console.log( this['is'], "ready!")
+    }
+
+}
+
+MicrotomeApp.register();
--- app/scripts/app.ts
+++ app/scripts/app.ts
@@ -20,55 +20,5 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   //   document.querySelector('#caching-complete').show();
   // };
 
-  // Listen for template bound event to know when bindings
-  // have resolved and content has been stamped to the page
-  app.addEventListener('dom-change', function() {
-    console.log('Our app is ready to rock!');
-  });
-
-  // See https://github.com/Polymer/polymer/issues/1381
-  window.addEventListener('WebComponentsReady', function() {
-    // imports are loaded and elements have been registered
-  });
-
-  // // Main area's paper-scroll-header-panel custom condensing transformation of
-  // // the appName in the middle-container and the bottom title in the bottom-container.
-  // // The appName is moved to top and shrunk on condensing. The bottom sub title
-  // // is shrunk to nothing on condensing.
-  // addEventListener('paper-header-transform', function(e) {
-  //   var appName = document.querySelector('.app-name');
-  //   var middleContainer = document.querySelector('.middle-container');
-  //   var bottomContainer = document.querySelector('.bottom-container');
-  //   var detail = e.detail;
-  //   var heightDiff = detail.height - detail.condensedHeight;
-  //   var yRatio = Math.min(1, detail.y / heightDiff);
-  //   var maxMiddleScale = 0.50; // appName max size when condensed. The smaller the number the smaller the condensed size.
-  //   var scaleMiddle = Math.max(maxMiddleScale, (heightDiff - detail.y) / (heightDiff / (1 - maxMiddleScale)) + maxMiddleScale);
-  //   var scaleBottom = 1 - yRatio;
-  //
-  //   // Move/translate middleContainer
-  //   Polymer.Base.transform('translate3d(0,' + yRatio * 100 + '%,0)', middleContainer);
-  //
-  //   // Scale bottomContainer and bottom sub title to nothing and back
-  //   Polymer.Base.transform('scale(' + scaleBottom + ') translateZ(0)', bottomContainer);
-  //
-  //   // Scale middleContainer appName
-  //   Polymer.Base.transform('scale(' + scaleMiddle + ') translateZ(0)', appName);
-  // });
-
-  // // Close drawer after menu item is selected if drawerPanel is narrow
-  // app['onMenuSelect'] = function() {
-  //   var drawerPanel = document.querySelector('#paperDrawerPanel');
-  //   if (drawerPanel['narrow'] === true) {
-  //     drawerPanel['closeDrawer']();
-  //   }
-  // };
-
-  app['onMenuActivate'] = function(event: Event, detail: Object) {
-    var menu = Polymer.dom(event).localTarget;
-    console.log(menu);
-    var name = detail['selected'];
-    menu.select(-1);
-  }
 
 })(document);
