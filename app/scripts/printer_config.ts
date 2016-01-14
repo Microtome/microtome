2537ee117242fb6cf11074d33f0c1dbcd448b426
Daniel Joyce <daniel.a.joyce@gmail.com>
2016-01-13 20:22:36 -0800

    Adding skeleton of printer config class

---
+++ app/scripts/printer_config.ts
@@ -0,0 +1,38 @@
+module microtome.printer {
+
+  /**
+  * For printers driven directly via GPIO, descrbes what each pin does
+  */
+  export interface GpioProtocol {
+
+  }
+
+  /**
+  * Configuration for GCode driven printers
+  */
+  export interface GcodeProtocol {
+  }
+
+  export interface PrintVolume {
+    width: number,
+    depth: number,
+    height:number
+  }
+
+  export interface PrinterConfig {
+    /** name of printer */
+    name: string;
+    /** description of printer */
+    description: string;
+    /** ms since epoch */
+    lastModified: number;
+    /**
+    * Print volume
+    * x = width
+    * y = depth
+    * z = height
+    */
+    volume: PrintVolume;
+    // protocol: GpioProtocol | GcodeProtocol
+  }
+}
