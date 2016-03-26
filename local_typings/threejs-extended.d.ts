c188375fb24beae6f6b2324bd38f4f89cb800d5c
Daniel Joyce <daniel.a.joyce@gmail.com>
2016-03-25 21:09:19 -0700

    Fix type warnings

--- local_typings/threejs-extended.d.ts
+++ local_typings/threejs-extended.d.ts
@@ -1,28 +1,18 @@
 // Required due to typescript bug 6595
-// <reference path="../typings/threejs/three.d.ts" />
-
-// import THREE from "../typings/threejs/three.d.ts";
-
-// declare module THREE_ex {
+/// <reference path="../typings/threejs/three.d.ts" />
 
 declare module THREE {
   export class STLLoader extends THREE.Loader {
     constructor(manager?: LoadingManager);
     manager: LoadingManager;
     withCredentials: boolean;
-
-    load(url: string, onLoad?: (geometry: Geometry) => void, onProgress?: (event: any) => void, onError?: (event: any) => void): void;
-
+    load(url: string, onLoad?: (geometry: THREE.Geometry) => void, onProgress?: (event: any) => void, onError?: (event: any) => void): void;
   }
 
   export class OBJLoader extends THREE.Loader {
     constructor(manager?: LoadingManager);
     manager: LoadingManager;
     withCredentials: boolean;
-
-    load(url: string, onLoad?: (geometry: Geometry) => void, onProgress?: (event: any) => void, onError?: (event: any) => void): void;
-
-
+    load(url: string, onLoad?: (geometry: THREE.Geometry) => void, onProgress?: (event: any) => void, onError?: (event: any) => void): void;
   }
 }
-// }
