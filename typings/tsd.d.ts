de3899e14b796139de1b6f28bb9ed2db2868f54d
Daniel Joyce <daniel.a.joyce@gmail.com>
2016-04-05 22:51:45 -0700

    Cleaned out old typings

--- typings/tsd.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="chrome/chrome-app.d.ts" />
-/// <reference path="filesystem/filesystem.d.ts" />
-/// <reference path="filewriter/filewriter.d.ts" />
-/// <reference path="q/Q.d.ts" />
-/// <reference path="threejs/three.d.ts" />
-/// <reference path="webcomponents.js/webcomponents.js.d.ts" />
