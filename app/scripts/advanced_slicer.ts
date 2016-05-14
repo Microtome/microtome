d2ebd91125a46dba92351053567b22ff6a815e58
Daniel Joyce <daniel.a.joyce@gmail.com>
2016-05-14 15:40:14 -0700

    More progress

--- app/scripts/advanced_slicer.ts
+++ app/scripts/advanced_slicer.ts
@@ -1,25 +1,33 @@
 module microtome.slicer {
 
+  // TODO Move to threed?
+  const Z_DOWN = new THREE.Vector3(0, 0, -1000000);
+  const Z_UP = new THREE.Vector3(0, 0, 1000000);
+
   /**
   Advanced slicer supporting shelling, support pattern generation,
   hollow shells, etc.
   */
   export class AdvancedSlicer {
+    // Slice camera
+    private _sliceCamera: THREE.OrthographicCamera = null;
+    // z-shell camera
+    private _zShellCamera: THREE.OrthographicCamera = null;
 
     // Final composition target where final image is composed
-    finalCompositeTarget: THREE.WebGLRenderTarget = null;
+    private finalCompositeTarget: THREE.WebGLRenderTarget = null;
 
     // Because of how we slice, we can't use stencil buffer
-    maskTarget: THREE.WebGLRenderTarget = null;
+    private maskTarget: THREE.WebGLRenderTarget = null;
 
     // Depth target
-    depthTarget: THREE.WebGLRenderTarget = null;
+    private depthTarget: THREE.WebGLRenderTarget = null;
 
     // Temp target 1
-    tempTarget1: THREE.WebGLRenderTarget = null;
+    private tempTarget1: THREE.WebGLRenderTarget = null;
 
     // Temp target 2
-    tempTarget2: THREE.WebGLRenderTarget = null;
+    private tempTarget2: THREE.WebGLRenderTarget = null;
 
     // Cache canvas width for dirty checking
     private _lastWidth: number = -1;
@@ -59,8 +67,6 @@ module microtome.slicer {
     };
 
     private _copyMaterialUniforms = {
-      'dialate': { type: 'i', value: 0 },
-      'pixelRadius': { type: 'f', value: AdvancedSlicer._MIN_SHELL_PIXELS },
       'src': { type: 't', value: <THREE.WebGLRenderTarget>null },
       'viewWidth': { type: 'i', value: 0.0 },
       'viewHeight': { type: 'i', value: 0.0 }
@@ -81,6 +87,8 @@ module microtome.slicer {
       planeMaterial.side = THREE.DoubleSide;
       this._sliceBackground = new THREE.Mesh(planeGeom, planeMaterial);
       this._sliceBackground.position.z = SLICER_BACKGROUND_Z;
+      this._sliceCamera = new THREE.OrthographicCamera(-0.5, 0.5, 0.5, -0.5);
+      this._zShellCamera = new THREE.OrthographicCamera(-0.5, 0.5, 0.5, -0.5);
     }
 
     /**
@@ -90,16 +98,21 @@ module microtome.slicer {
       this._render(z);
     }
 
+    /***
+    * Slice to an image
+    * Returns a dataurl of the image
+    */
     sliceAtToImage(z: number): String {
       this.sliceAt(z);
       return this.renderer.domElement.toDataURL();
     }
 
     private _render(z: number) {
+      let dirty = this._prepareRender();
       if (z <= this.raftThickness) {
         this._renderRaftSlice();
       } else {
-        this._renderSlice(z);
+        this._renderSlice(z, dirty);
       }
     }
 
@@ -110,34 +123,76 @@ module microtome.slicer {
       // Hide slice background if present
       this.scene.remove(this._sliceBackground);
       // render to texture
-      // Apply dialate filter
+      this.renderer.render(this.scene, this._sliceCamera, this.tempTarget1, true)
+      // Apply dialate filter to texture
+
       // Show slice background
       this.scene.add(this._sliceBackground);
-      // render.
+      // render texture to view
     }
 
-    _renderSlice(z: number) {
-
+    /**
+    * Handle reallocating render targets if dimensions have changed and
+    * return true if changes have occured
+    */
+    private _prepareRender(): boolean {
+      let dirty = false;
       // Get canvasElement height
-      var width = this.renderer.domElement.width;
-      var height = this.renderer.domElement.height;
+      let width = this.renderer.domElement.width;
+      let height = this.renderer.domElement.height;
       // If its changed...
       if (width != this._lastWidth || height != this._lastHeight) {
         // Recalc pixel width, and shelling parameters
         this._pixelWidthMM = this.scene.printVolume.width / width;
         this._pixelHeightMM = this.scene.printVolume.depth / height;
-        this._recalcShellSElemSampOffsets();
-        this._recalcRaftSElemSampOffsets();
+        // TODO NOT needed for now, will need to reapproach for non square pixel densities
+        // Also currently all done in the shader.
+        // this._recalcShellSElemSampOffsets();
+        // this._recalcRaftSElemSampOffsets();
         // Dispose old textures
-        this._disposeTargets();
         // Allocate new textures
-        this._allocateTargets(width, height);
-        // Generate depth image
-
-
+        this._reallocateTargets(width, height)
         this._lastWidth = width;
         this._lastHeight = height;
+        dirty = true;
       }
+      return dirty;
+    }
+
+    /**
+    * Update camera dimensions
+    */
+    private _prepareCameras(newWidth: number, newHeight: number) {
+      var pVolumeBBox = this.scene.printVolume.boundingBox;
+
+      var widthRatio: number = Math.abs(pVolumeBBox.max.x - pVolumeBBox.min.x) / newWidth;
+      var heightRatio: number = Math.abs(pVolumeBBox.max.y - pVolumeBBox.min.y) / newHeight;
+      var scale: number = widthRatio > heightRatio ? widthRatio : heightRatio;
+      let right = (scale * newWidth) / 2.0;
+      let left = -right;
+      let top = (scale * newHeight) / 2.0;
+      let bottom = -top
+      for (let camera of [this._sliceCamera, this._zShellCamera]) {
+        camera.right = right;
+        camera.left = left;
+        camera.top = top;
+        camera.bottom = bottom;
+        camera.updateProjectionMatrix();
+        camera.lookAt(Z_DOWN);
+      }
+    }
+
+    /**
+    * Update shader uniforms such as dimensions
+    */
+    private _prepareShaders(newWidth: number, newHeight: number) {
+
+    }
+
+    private _renderSlice(z: number, dirty: boolean) {
+
+      // Generate depth image
+
       // Generate slice mask image
 
       // Generate slice image with support
@@ -157,29 +212,36 @@ module microtome.slicer {
       // 3 Write combined texture, masked to final image
     }
 
-    private _recalcShellSElemSampOffsets() {
-      this._shellPixelsX = this.shellThickness / this._pixelWidthMM;
-      this._shellPixelsY = this.shellThickness / this._pixelHeightMM;
-      if (this._shellPixelsX < AdvancedSlicer._MIN_SHELL_PIXELS) {
-        window.console.warn(`Too few x shell pixels: ${this._shellPixelsX}`);
-        this._shellPixelsX = AdvancedSlicer._MIN_SHELL_PIXELS;
-      }
-      if (
-        this._shellPixelsY < AdvancedSlicer._MIN_SHELL_PIXELS) {
-        window.console.warn(`Too few y shell pixels: ${this._shellPixelsY}`);
-        this._shellPixelsX = AdvancedSlicer._MIN_SHELL_PIXELS;
-      }
-
-    }
-
-    private _recalcRaftSElemSampOffsets() {
-
-    }
+    // private _recalcShellSElemSampOffsets() {
+    //   this._shellPixelsX = this.shellThickness / this._pixelWidthMM;
+    //   this._shellPixelsY = this.shellThickness / this._pixelHeightMM;
+    //   if (this._shellPixelsX < AdvancedSlicer._MIN_SHELL_PIXELS) {
+    //     window.console.warn(`Too few x shell pixels: ${this._shellPixelsX}`);
+    //     this._shellPixelsX = AdvancedSlicer._MIN_SHELL_PIXELS;
+    //   }
+    //   if (
+    //     this._shellPixelsY < AdvancedSlicer._MIN_SHELL_PIXELS) {
+    //     window.console.warn(`Too few y shell pixels: ${this._shellPixelsY}`);
+    //     this._shellPixelsX = AdvancedSlicer._MIN_SHELL_PIXELS;
+    //   }
+    //
+    // }
+    //
+    // private _recalcRaftSElemSampOffsets() {
+    //
+    // }
 
     /**
-    * Allocate the rendering Targets
+    * reallocate the rendering Targets
     */
-    private _allocateTargets(width: number, height: number) {
+    private _reallocateTargets(width: number, height: number) {
+      // Dispose
+      this.finalCompositeTarget.dispose();
+      this.depthTarget.dispose();
+      this.maskTarget.dispose();
+      this.tempTarget1.dispose();
+      this.tempTarget2.dispose();
+      // Allocate
       this.finalCompositeTarget = new THREE.WebGLRenderTarget(width, height, {
         format: THREE.RGBAFormat,
         depthBuffer: true,
@@ -191,6 +253,16 @@ module microtome.slicer {
         wrapT: THREE.ClampToEdgeWrapping
       });
       this.maskTarget = new THREE.WebGLRenderTarget(width, height, {
+        format: THREE.RGBAFormat,
+        depthBuffer: true,
+        stencilBuffer: false,
+        // generateMipMaps: false,
+        minFilter: THREE.NearestFilter,
+        magFilter: THREE.NearestFilter,
+        wrapS: THREE.ClampToEdgeWrapping,
+        wrapT: THREE.ClampToEdgeWrapping
+      });
+      this.depthTarget = new THREE.WebGLRenderTarget(width, height, {
         format: THREE.RGBAFormat,
         depthBuffer: true,
         stencilBuffer: false,
@@ -235,11 +307,5 @@ module microtome.slicer {
     /**
     * Dispose of all rendering targets
     */
-    private _disposeTargets() {
-      this.finalCompositeTarget.dispose();
-      this.maskTarget.dispose();
-      this.tempTarget1.dispose();
-      this.tempTarget2.dispose();
-    }
   }
 }
