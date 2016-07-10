87a70136cace53756e2d05156363b965e3df56ac
Daniel Joyce <daniel.a.joyce@gmail.com>
2016-07-09 19:02:19 -0700

    Add OR image material

--- app/scripts/core_threed.ts
+++ app/scripts/core_threed.ts
@@ -48,7 +48,7 @@ module microtome.three_d {
     ) { }
   }
 
-  export class XorShaderUniforms {
+  export class BoolOpShaderUniforms {
     constructor(public src1: TextureUniform,
       public src2: TextureUniform,
       public viewWidth: IntegerUniform,
@@ -143,6 +143,28 @@ void main(void) {
   // float dst = round(smpl1) ^ round(smpl2);
   float dst = 0.0;
   if((smpl1 > 0.9 || smpl2 > 0.9) && !(smpl1 > 0.9 && smpl2 > 0.9)){
+    dst = 1.0;
+  }
+  gl_FragColor = vec4(vec3(dst),1);
+}  `;
+
+    private static _orShaderFrag = `
+// Two sources to xor together
+uniform sampler2D src1;
+uniform sampler2D src2;
+
+// View dimensions
+uniform int viewWidth;
+uniform int viewHeight;
+
+void main(void) {
+  vec2 lookup = gl_FragCoord.xy / vec2(viewWidth, viewHeight );
+  float smpl1 = texture2D(src1, lookup).r;
+  float smpl2 = texture2D(src2, lookup).r;
+  // No OR yet? :P
+  // float dst = round(smpl1) || round(smpl2);
+  float dst = 0.0;
+  if(smpl1 > 0.9 || smpl2 > 0.9){
     dst = 1.0;
   }
   gl_FragColor = vec4(vec3(dst),1);
@@ -294,6 +316,17 @@ void main(void) {
     */
     static xorMaterial = new THREE.ShaderMaterial({
       fragmentShader: CoreMaterialsFactory._xorShaderFrag,
+      vertexShader: CoreMaterialsFactory._basicVertex,
+      side: THREE.FrontSide,
+      blending: THREE.AdditiveBlending,
+      uniforms: {}
+    });
+
+    /**
+    Material for or
+    */
+    static orMaterial = new THREE.ShaderMaterial({
+      fragmentShader: CoreMaterialsFactory._orShaderFrag,
       vertexShader: CoreMaterialsFactory._basicVertex,
       side: THREE.FrontSide,
       blending: THREE.AdditiveBlending,
