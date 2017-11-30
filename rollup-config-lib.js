fc05e074424aec9c73c7536d23b45833f1041a6e
Daniel Joyce <daniel.joyce@optimumenergyco.com>
2017-11-29 22:06:04 -0800

    Fix up rollup lib

--- rollup-config-lib.js
+++ rollup-config-lib.js
@@ -2,17 +2,17 @@ import rollupString from 'rollup-plugin-string';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 
 export default {
-    entry: 'build/lib/js/index.js',
+    entry: 'build/lib/js/microtome/index.js',
     format: 'es',
     external: ['three', 'jszip'],
-    dest: 'dist/lib/microtome-lib.js',
+    dest: 'dist/lib/microtome.js',
     sourceMap: true,
     moduleName: "microtome",
     plugins: [
         // customResolver(),
         rollupString({
             // Required to be specified
-            include: 'build/lib/js/**/*.glsl',
+            include: 'build/lib/js/microtome/**/*.glsl',
         }),
         sourcemaps()
     ],
