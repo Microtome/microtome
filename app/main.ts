3273d1a7ce7db30671d8ac1f80d7d881687ccb4b
Daniel Joyce <daniel.joyce@optimumenergyco.com>
2017-12-05 16:18:06 -0800

    Get slicing working in sample app

--- app/main.ts
+++ app/main.ts
@@ -3,6 +3,8 @@ import { SlicePreview } from "./slicePreview";
 import * as microtome from "microtome";
 import { SphereGeometry, Mesh } from "three";
 
+let PrintMesh = microtome.three_d.PrintMesh;
+
 let PrinterScene = microtome.three_d.PrinterScene;
 let CoreMaterialsFactory = microtome.three_d.CoreMaterialsFactory;
 
@@ -13,15 +15,29 @@ console.log(PrintVolViewDiv);
 console.log(SlicePreviewDiv);
 
 let printerScene = new PrinterScene();
-let pvv = new microtome.three_d.PrintVolumeView(320, 240, 120);
-let sphere1 = new Mesh(new SphereGeometry(10, 16, 16), CoreMaterialsFactory.objectMaterial)
+printerScene.printVolume.resize(128, 96, 96);
+let sphere1 = new PrintMesh(new SphereGeometry(10, 16, 16), CoreMaterialsFactory.objectMaterial)
 sphere1.position.set(15, 15, 15);
-let sphere2 = new Mesh(new SphereGeometry(10, 16, 16), CoreMaterialsFactory.objectMaterial)
+let sphere2 = new PrintMesh(new SphereGeometry(10, 16, 16), CoreMaterialsFactory.objectMaterial)
 sphere2.position.set(15, 24, 20);
-let sphere3 = new Mesh(new SphereGeometry(15, 16, 16), CoreMaterialsFactory.objectMaterial)
+let sphere3 = new PrintMesh(new SphereGeometry(15, 16, 16), CoreMaterialsFactory.objectMaterial)
 sphere3.position.set(15, 23, 35);
 
-printerScene.add(pvv, sphere1, sphere2, sphere3);
+// Add some dummy objects
+printerScene.printObjects.push(sphere1);
+printerScene.printObjects.push(sphere2);
+printerScene.printObjects.push(sphere3);
 
 var pvView = new PrinterVolumeView(PrintVolViewDiv, printerScene);
 var slicePreview = new SlicePreview(SlicePreviewDiv, printerScene);
+var sliceAtSlider = <HTMLInputElement>document.getElementById("slice-at");
+sliceAtSlider.min = "0";
+sliceAtSlider.max = "96";
+sliceAtSlider.step = "0.1";
+sliceAtSlider.value = "25";
+slicePreview.sliceAt = 25;
+sliceAtSlider.oninput = (e: Event) => {
+    slicePreview.sliceAt = parseInt((<HTMLInputElement>e.target).value, 10)
+};
+
+
