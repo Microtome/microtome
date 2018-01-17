f05a043115427f9ada8978eef95d24d0d6bd31ea
Daniel Joyce <daniel.a.joyce@gmail.com>
2018-01-17 06:46:41 -0800

    Update rollup version

--- rollup-config-lib.js
+++ rollup-config-lib.js
@@ -2,12 +2,19 @@ import rollupString from 'rollup-plugin-string';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 
 export default {
-    entry: 'build/lib/microtome/index.js',
-    format: 'es',
+    input: 'build/lib/microtome/index.js',
+    output:{
+        name: "microtome",
+        format: 'es',
+        file: 'dist/lib/microtome.js',
+        paths: {
+            "three": "/lib/three",
+            "jszip": "/lib/jszip",
+            "file-saver": "/lib/file-saver"
+        },
+        sourcemap: true,
+    },
     external: ['three', 'jszip'],
-    dest: 'dist/lib/microtome.js',
-    sourceMap: true,
-    moduleName: "microtome",
     plugins: [
         rollupString({
             // Required to be specified
@@ -15,9 +22,4 @@ export default {
         }),
         sourcemaps()
     ],
-    paths: {
-        "three": "/lib/three",
-        "jszip": "/lib/jszip",
-        "file-saver": "/lib/file-saver"
-    }
 };
