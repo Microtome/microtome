8ee4453edee48164b9259e8f6852f451a7d4802d
Daniel Joyce <daniel.a.joyce@gmail.com>
2018-01-18 06:44:00 -0800

    fix: Namespace exports to organize them

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,12 @@
 
-export * from "./camera";
-export * from "./config";
-export * from "./job";
-export * from "./materials";
-export * from "./printer";
-export * from "./slicer";
+import * as camera from "./camera";
+import * as config from "./config";
+import * as job from "./job";
+import * as materials from "./materials";
+import * as printer from "./printer";
+import * as slicer from "./slicer";
+import * as units from "./units";
+
+export {
+    camera, config, job, materials, printer, slicer, units
+}
